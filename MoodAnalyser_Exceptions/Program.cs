d097a40d478599003185a13b73bc32bf45ad0bc0
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-14 13:48:03 +0530

    Added Welcome Message

---
+++ MoodAnalyser_Exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyser_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Mood Analyser Exception Problem");
+        }
+    }
+}
